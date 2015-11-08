b09d2729fb5bd5560b70b0fc6cfa02a2ba466e48
bushra-qadir <bushraqadir@ciit.net.pk>
2015-11-08 21:55:45 +0500

    added a comment

--- scm_lab_demo/src/scm_lab_demo/Main_class.java
+++ scm_lab_demo/src/scm_lab_demo/Main_class.java
@@ -5,6 +5,7 @@ public class Main_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Let's do something using GIT.");
+		//lets add a comment
 
 	}
 
