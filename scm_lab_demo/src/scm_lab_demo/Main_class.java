37fbd63d3f8a03f2099bac0a1e53f80e296f7bd1
bushra-qadir <bushraqadir@ciit.net.pk>
2015-11-08 21:45:32 +0500

    My first commit

---
+++ scm_lab_demo/src/scm_lab_demo/Main_class.java
@@ -0,0 +1,11 @@
+package scm_lab_demo;
+
+public class Main_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's do something using GIT.");
+
+	}
+
+}
